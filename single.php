068d1412dd4bb4f7bd87300eaf278d5736c8c064
the-duck <markspurgeon96@hotmail.com>
2019-03-02 09:50:51 +0100

    utf-8 encoding -> require removing debug due to kses warnings

--- single.php
+++ single.php
@@ -176,23 +176,12 @@ get_header();
 
 					<div class="entry-content">
 						<?php
-						the_content( sprintf(
-							wp_kses(
-								/* translators: %s: Name of current post. Only visible to screen readers */
-								__( 'Continue reading<span class="screen-reader-text"> "%s"</span>', 'topolitik' ),
-								array(
-									'span' => array(
-										'class' => array(),
-									),
-								)
-							),
-							get_the_title()
-						) );
+						echo mb_convert_encoding(get_the_content(), 'utf8'); /* replaced from 'the_content' to allow utf-8 encoding*/
 
-						wp_link_pages( array(
+						/*wp_link_pages( array(
 							'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'topolitik' ),
 							'after'  => '</div>',
-						) );
+						) );*/
 						?>
 					</div><!-- .entry-content -->
 
