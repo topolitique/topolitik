3bb00b2e7aa8f9e730dd6e67baf42924d905f54d
the-duck <markspurgeon96@hotmail.com>
2019-11-05 18:51:42 +0100

    search button

--- home.php
+++ home.php
@@ -1,4 +1,15 @@
 <?php
+/**
+ * Template Name: Home
+ * 
+ * The template for displaying the footer
+ *
+ * Contains the closing of the #content div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package topolitik
+ */
 get_header();
 ?>
 
